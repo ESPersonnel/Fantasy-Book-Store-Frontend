eca690804401d2609a46a039c42cc6b9d2a961ff
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-09-13 21:57:06 +0300

    Added Books component for displaying books.

---
+++ src/components/books/Book.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Book(props, onDeleteBook) {
+
+  const { id, title, author, genre, publication_date } = props;
+  const [isfavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isfavorite);
+  }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:9292/books/${id}`, {
+      method: "DELETE"
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      onDeleteBook(id)
+    })
+  }
+
+  return (
+    <div className="book">
+      <Link to={`/books/${props.book.id}` }>
+        View Book
+      </Link>
+      <h3>Book: {props.book.title}</h3>
+      <p>Author: {props.book.author}</p>
+      <p>Genre: {props.book.genre}</p>
+      <p>Publication Date: {props.book.publication_date}</p>
+      <button onClick={toggleFavorite}>{isfavorite ? "Unfavorite" : "Favorite"}</button>
+      <button onClick={handleDeleteClick}>Delete</button>
+    </div>
+  );
+}
+
+
+export default Book;
